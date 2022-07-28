8ac4ca6c634233dfcc102ec5dc9db2937a07d746
nguyenphucminh <nguyenphucminh.lx@gmail.com>
2022-07-28 15:03:59 +0700

    feat: minh

--- reactApp/my-app/src/App.js
+++ reactApp/my-app/src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useReducer, useRef} from 'react'
+//1.Init state
+const initState = {job:'', jobs: []}
+//2.Action
+const SET_JOB = 'set_job'
+const ADD_JOB = 'add_job'
+const DELETE_JOB = 'delete_job'
+//payload là dữ liệu mang theo đi, nên user gõ vào input là payload sẽ mang theo
+const setJob = payload =>{
+  return {
+    type: SET_JOB,
+    payload
+  }
+}
+const addJob = payload =>{
+  return {
+    type: ADD_JOB,
+    payload
+  } 
+}
+const deleteJob = payload =>{
+  return {
+    type: DELETE_JOB,
+    payload
+  }
+}
+//3.Reducer
+const reducer = (state, action)=>{
+  let newState
+  switch(action.type)
+  {
+    case SET_JOB:
+      newState = {
+        ...state,
+        job: action.payload
+      }
+      break
+    case ADD_JOB:
+      newState = {
+        ...state,
+        jobs:[...state.jobs, action.payload]
+      }
+      break
+    case DELETE_JOB:
+      const newJobs = [...state.jobs]
+      newJobs.splice(action.payload,1)
+      newState = {
+        ...state,
+        jobs: newJobs
+      }
+      break
+    default:
+      throw new Error('Invallid action')
+  }
+  return newState
+}
 function App() {
+  const [state, dispatch] =  useReducer(reducer, initState);
+  const {job, jobs} = state
+  const inputRef = useRef()
+  const handleSubmit = () =>{
+    dispatch(addJob(job))
+    dispatch(setJob(''))  
+    inputRef.current.focus()
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>To Do App</h3>
+      <input
+        ref={inputRef}
+        value={job}
+        placeholder='Enter Todo'
+        onChange={e =>{
+          dispatch(setJob(e.target.value))
+        }}
+      />
+      <button onClick={handleSubmit}>Add</button>
+      <ul>
+        {jobs.map((job, index)=>(
+          <li key={index}>{job}{'    '}   
+            <button onClick={()=>dispatch(deleteJob(index))}>&times;</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
